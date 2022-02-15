e9c5e548ab79e7dab4826ff89ae7a6a0b9a8c676
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-16 01:56:24 +0530

    Added : Check Employee Attendance

--- src/com/blz/java/EmployeeWageComputationProgram.java
+++ src/com/blz/java/EmployeeWageComputationProgram.java
@@ -1,7 +1,26 @@
 package com.blz.java;
 
-public class EmployeeWageComputationProgram {
-	public static void main(String []Args) {
-	System.out.println("Welcome to Employee Wage Computation Program Object Orinted Programing");
+import java.util.Random;
+
+class Employee {
+    static final int IS_FULL_TIME = 1;
+
+    public static void empCheck() {
+	Random random = new Random();
+	int empCheck = random.nextInt(2);
+	if (empCheck == IS_FULL_TIME) {
+	    System.out.println("PRESENT");
+	} else {
+	    System.out.println("ABSENT");
 	}
+    }
 }
+
+public class EmployeeWageComputationProgram {
+
+    public static void main(String[] args) {
+	System.out.println("Welcome to Employee Wage Computation Program Object Orinted Program");
+	Employee.empCheck();
+    }
+}
+
