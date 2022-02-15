e86601cd21bfa8ab58eb15e520a83121e0d46806
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-16 01:42:57 +0530

    Added : Welcome Employee Wage Program

---
+++ src/com/blz/java/EmployeeWageComputationProgram.java
@@ -0,0 +1,7 @@
+package com.blz.java;
+
+public class EmployeeWageComputationProgram {
+	public static void main(String []Args) {
+	System.out.println("Welcome to Employee Wage Computation Program Object Orinted Programing");
+	}
+}
