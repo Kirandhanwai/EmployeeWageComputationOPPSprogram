23a4e4107f65d96925fa6f4060417cd1c269cb39
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-16 02:00:12 +0530

    Added : Calculated Employee Daily Wage

--- src/com/blz/java/EmployeeWageComputationProgram.java
+++ src/com/blz/java/EmployeeWageComputationProgram.java
@@ -4,23 +4,32 @@ import java.util.Random;
 
 class Employee {
     static final int IS_FULL_TIME = 1;
+    static final int EMP_WAGE_PER_DAY = 20;
 
-    public static void empCheck() {
+    public static int empCheck() {
 	Random random = new Random();
 	int empCheck = random.nextInt(2);
+	return empCheck;
+    }
+
+    public static void empSalary(int empCheck) {
+	int empWrkHrs = 0;
 	if (empCheck == IS_FULL_TIME) {
-	    System.out.println("PRESENT");
+	    empWrkHrs = 8;
 	} else {
-	    System.out.println("ABSENT");
+	    empWrkHrs = 0;
 	}
+	int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
+	System.out.println("empWage = " + empWage);
     }
 }
 
 public class EmployeeWageComputationProgram {
 
     public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation Program Object Orinted Program");
-	Employee.empCheck();
+	System.out.println("Welcome to Employee Wage Computation Program Object Orinted Programing");
+	int empCheck = Employee.empCheck();
+	Employee.empSalary(empCheck);
     }
 }
 
